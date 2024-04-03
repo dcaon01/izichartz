c18b0ca485ed3d51df477809a33435239f30c864
damianocaon <d.caon01@gmail.com>
2024-04-03 14:13:48 +0200

    Il progetto funziona

--- components/modules/ER/Linker.js
+++ components/modules/ER/Linker.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, memo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { motion } from "framer-motion";
 import { elementsSlice } from "@/store/design/elements-slice";
