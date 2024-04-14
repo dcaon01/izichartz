5b950add954d676644f7c6a0fa815a59c852213b
damianocaon <d.caon01@gmail.com>
2024-04-14 17:13:37 +0200

    Sistemato un bug

--- components/modules/ER/EntityRelationship.js
+++ components/modules/ER/EntityRelationship.js
@@ -312,7 +312,7 @@ export const EntityRelationship = memo(function EntityRelationship({ id, type, o
      */
     function handleInput(event) {
         let oldEffectiveWidth = size.width - text.width;
-        let newHeight = type === "relationship" ? (inputRef.current.getBoundingClientRect().height * size.width) / (2 * (size.width - inputRef.current.getBoundingClientRect().width)) + 80 : size.height;
+        let newHeight = (inputRef.current.getBoundingClientRect().height * size.width) / (2 * (size.width - inputRef.current.getBoundingClientRect().width)) + 80;
         console.log(type === "relationship");
         dispatch(elementsSlice.actions.setConnectingElement(0));
         dispatch(elementsSlice.actions.modifyElementOptions({ id: id, option: "text", value: { value: event.target.value, width: inputRef.current.getBoundingClientRect().width } }));
@@ -321,15 +321,16 @@ export const EntityRelationship = memo(function EntityRelationship({ id, type, o
             option: "size",
             value: {
                 width: oldEffectiveWidth + inputRef.current.getBoundingClientRect().width,
-                height: newHeight,
+                height: type === "relationship" ? newHeight : size.height,
             }
         }));
-        dispatch(elementsSlice.actions.modifyElementOptions({
-            id: id,
-            option: "minSize",
-            value: newHeight
-        }));
-        console.log(newHeight);
+        if (type === "relationship") {
+            dispatch(elementsSlice.actions.modifyElementOptions({
+                id: id,
+                option: "minSize",
+                value: newHeight
+            }));
+        }
     }
 
     function handleBlur(event) {
