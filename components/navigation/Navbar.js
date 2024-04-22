f8a5d68289c6cef82b08ff04862bbe86dae195c0
damianocaon <d.caon01@gmail.com>
2024-04-22 16:29:38 +0200

    Sistemato lo scheletro della navbar

--- components/navigation/Navbar.js
+++ components/navigation/Navbar.js
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState } from "react";
 import classes from "./Navbar.module.css";
+import { motion } from "framer-motion";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { robotoMono } from "@/app/fonts.js";
@@ -19,28 +20,23 @@ export default function Navbar() {
 
     return (
         <nav className={classes.navbar}>
-            <Image src="/assets/global/logo.png" className={classes.logo} width={176} height={27} alt="Back to Home" />
-            <span className={classes.whiteSpace} />
+            <Link className={classes.logo} href="/">
+                {/* Dimensioni originali: w=878, h=135*/}
+                <img src="/assets/global/logo.png" height={30} width={198} alt="Back to Home" />
+            </Link>
+            {/*<span className={classes.whiteSpace} />*/}
             <div className={classes.links}>
                 <div>
-                    <button onClick={handleModulesDropDown}>Modules</button>
-
+                    <button onClick={handleModulesDropDown} className={`${classes.link} ${robotoMono.className}`}>Modules</button>
                 </div>
                 {modulesDropDown && /* renderizza il dropdown */ null}
                 <Link className={`${classes.link} ${robotoMono.className}`} href="">Plans</Link>
-                <Link className={classes.link} href="">Contacts</Link>
-                <Link className={classes.link} href="">About</Link>
-                <Link className={classes.link} href="">Login</Link>
+                <Link className={`${classes.link} ${robotoMono.className}`} href="">Contacts</Link>
+                <Link className={`${classes.link} ${robotoMono.className}`} href="">About</Link>
+                <Link className={`${classes.link} ${robotoMono.className}`} href="">Login</Link>
                 <Link
-                    className={classes.link}
+                    className={`${classes.navBotton} ${robotoMono.className}`}
                     href=""
-                    style={{
-                        backgroundColor: "black",
-                        color: "white",
-                        paddingLeft: 15,
-                        paddingRight: 15,
-                        borderRadius: 15,
-                    }}
                 >
                     Register
                 </Link>
