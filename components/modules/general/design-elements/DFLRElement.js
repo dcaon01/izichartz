b2c1b6c8b6d4c2ee79578aa02f0e09fb08423fca
damianocaon <d.caon01@gmail.com>
2024-04-25 15:44:25 +0200

    Sistemati alcuni CSS di tailwind che mi avrebbero potuto sminchiare il resto, ma ora no

--- components/modules/general/design-elements/DFLRElement.js
+++ components/modules/general/design-elements/DFLRElement.js
@@ -1,6 +1,6 @@
 'use-client';
 
-import classes from "./RDFLElements.module.css";
+import classes from "./DFLRElements.module.css";
 import { motion } from "framer-motion";
 import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
