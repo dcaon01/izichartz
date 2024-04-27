2ee511d7b06759b2ebc547d8e7f04984869dcbcb
damianocaon <d.caon01@gmail.com>
2024-04-27 16:19:14 +0200

    Animazione Navbar Sistemata

--- components/navigation/MobileNavbar.js
+++ components/navigation/MobileNavbar.js
@@ -4,60 +4,77 @@ import classes from "./MobileNavbar.module.css";
 import Link from "next/link";
 import { useState } from "react";
 import { robotoMono } from "@/app/fonts.js";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useAnimate } from "framer-motion";
 
 export default function MobileNavbar() {
     let [isMenuDroppedDown, setMenuDropDown] = useState(false);
+    let [scope, animate] = useAnimate();
 
     function handleMenuDropDown() {
         if (isMenuDroppedDown) {
             setMenuDropDown(false);
+            animate("div", { opacity: 0 }, { duration: 0.3 });
         } else {
             setMenuDropDown(true);
         }
     }
 
+    function animateLinksPresence() {
+        animate("div", { opacity: 1 }, { duration: 0.3 });
+    }
+
     return (
-        <nav className={classes.navbar}>
+        <nav
+            className={classes.navbar}
+            style={{ 
+                height: "auto",
+            }}
+        >
             <div className={classes.headSection}>
                 <Link className={classes.logo} href="/">
                     {/* Dimensioni originali: w=181, h=135*/}
                     <img src="/assets/global/logo-mobile.png" height={30} width={41} alt="Back to Home" />
                 </Link>
                 <div className={classes.menu} onClick={handleMenuDropDown}>
-                    <span className={`${classes.line1} ${isMenuDroppedDown && classes.toggleLine1}`}/>
-                    <span className={`${isMenuDroppedDown && classes.toggleLine2}`}/>
-                    <span className={`${classes.line3} ${isMenuDroppedDown && classes.toggleLine3}`}/>
+                    <span className={`${classes.line1} ${isMenuDroppedDown && classes.toggleLine1}`} />
+                    <span className={`${isMenuDroppedDown && classes.toggleLine2}`} />
+                    <span className={`${classes.line3} ${isMenuDroppedDown && classes.toggleLine3}`} />
                 </div>
-                <AnimatePresence>
+            </div>
+            <AnimatePresence wait>
                 {
                     isMenuDroppedDown &&
-                    <motion.div className={classes.links}
+                    <motion.div 
                         initial={{
-                            height: 0
+                            margin: 0,
+                            height: 0,
                         }}
                         animate={{
-                            height: "auto"
+                            margin: 20,
+                            height: "auto",
                         }}
                         exit={{
-                            height: 0
+                            margin: 0,
+                            height: 0,
                         }}
+                        onAnimationComplete={animateLinksPresence}
+                        ref={scope}
                     >
-                        <Link className={`${classes.link} ${robotoMono.className}`} href="">Plans</Link>
-                        <Link className={`${classes.link} ${robotoMono.className}`} href="">Contacts</Link>
-                        <Link className={`${classes.link} ${robotoMono.className}`} href="">About</Link>
-                        <Link className={`${classes.link} ${robotoMono.className}`} href="">Login</Link>
-                        <Link
-                            className={`${classes.navBotton} ${robotoMono.className}`}
-                            href=""
-                        >
-                            Register
-                        </Link>
+                        <div className={classes.links} style={{ opacity: 0 }}>
+                            <Link className={`${classes.link} ${robotoMono.className}`} href="">Plans</Link>
+                            <Link className={`${classes.link} ${robotoMono.className}`} href="">Contacts</Link>
+                            <Link className={`${classes.link} ${robotoMono.className}`} href="">About</Link>
+                            <Link className={`${classes.link} ${robotoMono.className}`} href="">Login</Link>
+                            <Link
+                                className={`${classes.navBotton} ${robotoMono.className}`}
+                                href=""
+                            >
+                                Register
+                            </Link>
+                        </div>
                     </motion.div>
                 }
-                </AnimatePresence>
-            </div>
-
+            </AnimatePresence>
         </nav>
     );
 };
