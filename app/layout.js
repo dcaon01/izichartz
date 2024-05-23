173e573efc3633a6e66720f6ba96ecf9651e81de
damianocaon <d.caon01@gmail.com>
2024-05-23 23:18:45 +0200

    Sistemata l'indicizzazione

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,11 @@ import "./globals.css";
 import Header from "@/components/global-layout/Header.js";
 import Footer from "@/components/global-layout/Footer.js";
 import { inter } from "./fonts.js";
-import { Suspense } from "react";
 
 export const metadata = {
   title: "Izichartz",
   description: "The best software design tool",
+  index: false,
 };
 
 export default function RootLayout({ children }) {
