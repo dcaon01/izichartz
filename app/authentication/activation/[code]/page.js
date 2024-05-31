eb789edf4c889da19a6c6842e551aa44fc201bac
damianocaon <d.caon01@gmail.com>
2024-05-31 11:48:30 +0200

    Attivazione dell'account completata, ora procedere con la sessione

--- app/authentication/activation/[code]/page.js
+++ app/authentication/activation/[code]/page.js
@@ -10,29 +10,29 @@ import { robotoMono } from "@/app/fonts";
 import { useFormState } from "react-dom";
 
 export default function ActivationPage({ params }) {
-    const [actError, userAccAct] = useFormState(userAccountActivation, { activationMessages: [] });
-    const [resError, resend] = useFormState(resendActivationCode, { resendMessages: [] });
-
+    const [accActError, userAccAct] = useFormState(userAccountActivation, { messages: [] });
+    const [resError, resend] = useFormState(resendActivationCode, { messages: [] });
+    
     return (
         <>
             <h1 className={`${robotoMono.className}`}>Activate</h1>
-            {actError.activationMessages.length > 0 && <ErrorDisplayer messages={actError.activationMessages} />}
+            {accActError.messages.length > 0 && <ErrorDisplayer messages={accActError.messages} />}
             <p className={`${robotoMono.className} ${classes.header}`}>
                 Insert here the code we sent to your email to activate your account.
             </p>
             <form className={classes.form} action={userAccAct}>
                 <AuthInput id="activation-code" type="text" label="Code" />
                 <button className={`${classes.submitButton} ${robotoMono.className}`} type="submit">Activate</button>
-                <input style={{ display: "none" }} id="activation-slug" value={params.code} readOnly />
+                <input style={{ display: "none" }} id="activation-slug" name="activation-slug" value={params.code} readOnly />
             </form>
-            <form className={specClasses.resend} action={resend}>
+            <form className={specClasses.resend} action={resend} >
                 <p className={`${robotoMono.className} ${specClasses.resendText}`}>
                     Didn't get the code? &nbsp;
                 </p>
                 <button className={`${specClasses.resendButton} ${robotoMono.className}`} type="submit">Resend</button>
                 <input style={{ display: "none" }} id="resend-activation-slug" value={params.code} readOnly />
             </form>
-            {resError.resendMessages.length > 0 && <ErrorDisplayer messages={resError.resendMessages} />}
+            {resError.messages.length > 0 && <ErrorDisplayer messages={resError.messages} />}
         </>
     );
 };
